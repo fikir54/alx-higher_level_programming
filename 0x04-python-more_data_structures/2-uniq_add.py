775eeac74c632ac3248184eb2af968019fe7879c
fikir54 <fikir.page@gmail.com>
2022-06-08 15:37:14 +0300

    Unique Add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_values = []
-    sum = 0
-    for elem in my_list:
-        if elem in unique_values:
-            continue
-        else:
-            unique_values.append(elem)
-    for item in unique_values:
-        sum += item
-    return
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
