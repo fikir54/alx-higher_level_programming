fecadafed0dbeb6939698d6395da69d237923dcb
fikir54 <fikir.page@gmail.com>
2022-06-10 18:17:41 +0300

    Square Matrix Simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-
-# square_matrix_simple - computes the square value of all ints if a matrix.
 def square_matrix_simple(matrix=[]):
-    lenM = len(matrix)
-    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(lenM)]
+    new_matrix = matrix.copy()
+
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j]*matrix[i][j]
-    return new_matrix 
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
